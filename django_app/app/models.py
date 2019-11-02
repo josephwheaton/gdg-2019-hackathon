237f02199af8149078ae23cabeffa0c86f51b3a9
Sam <sppike@uwm.edu>
2019-11-02 12:57:58 -0500

    target and model work

--- django_app/app/models.py
+++ django_app/app/models.py
@@ -2,13 +2,14 @@ from django.db import models
 
 
 class Location(models.Model):
+    location_id = models.AutoField(primary_key=True)
     # max length is arb. currently
     lat = models.FloatField()
     long = models.FloatField()
     name = models.CharField(max_length=50)
 
-    def create_Location(self, info):
-        Location.objects.create(self, Lat=info, long=info)
+    def create_Location(self, latitude, longitude):
+        Location.objects.create(self, Lat=latitude, long=longitude)
 
     def check_if_created(self, info):
         pass
@@ -19,14 +20,13 @@ class Target(models.Model):
     location = models.ForeignKey("Location")
     image = models.ImageField()
 
-
     # time this target started
     startTime = models.DateTimeField()
     # a default ammount currently
     points = models.IntegerField(default=20)
 
-    def create_target(self,loc, info):
-        Target.objects.create(self, Location=loc, image= info, points=info,)
+    def create_target(self, loc, info):
+        Target.objects.create(self, Location=loc, image=info, points=info,)
         pass
 
     # functionality: if same area is marked by different people can add pictures to the original  target,
@@ -35,4 +35,5 @@ class Target(models.Model):
         for e in Target.objects.all():
             if self.location.lat == e.location.lat and self.location.long == e.location.long:
                 # TODO currently only runs when its exactly the same location two targets/same location
-        pass
+                return True
+        return False
