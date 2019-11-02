1c5910fa53cb5ae88a1f01981b86644e56912c53
Sam <sppike@uwm.edu>
2019-11-02 11:36:13 -0500

    added to models.py

--- django_app/app/models.py
+++ django_app/app/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    # max length is arb. currently
+    lat = models.FloatField()
+    long = models.FloatField()
+    name = models.CharField(max_length=50)
+
+    def create_Location(self, info):
+        pass
+
+    def check_if_created(self, info):
+        pass
+
+
+class Target(models.Model):
+    location = Location()
+    image = models.ImageField()
+
+    # time this target started
+    startTime = models.DateTimeField()
+    # a default ammount currently
+    points = models.IntegerField(default=20)
+
+    def create_target(self,info):
+        Target.objects.create()
+        pass
+
+    # functionality: if same area is marked by different people can add pictures to the original  target,
+    # so no dupes.
+    def check_if_created(self, info):
+
+        pass
