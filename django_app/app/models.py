7d7387e878373e7b384f9a2c6a9f5385f220b68a
Sam <sppike@uwm.edu>
2019-11-02 14:15:31 -0500

    added some support methods

--- django_app/app/models.py
+++ django_app/app/models.py
@@ -6,7 +6,7 @@ class Location(models.Model):
     # max length is arb. currently
     lat = models.FloatField()
     long = models.FloatField()
-    name = models.CharField(max_length=50)
+
 
     def create_Location(self, latitude, longitude):
         Location.objects.create(self, Lat=latitude, long=longitude)
@@ -15,19 +15,40 @@ class Location(models.Model):
         pass
 
 
+#TODO make this function better. math is awfull.
+def in_range(e, self):
+    dif1 =e.location.lat-self.location.lat
+    dif2 =e.location.long-self.location.long
+    if abs(dif1) +abs(dif2) <20:
+         return True
+    return False
+
+
 class Target(models.Model):
     target_id = models.AutoField(primary_key=True)
     location = models.ForeignKey("Location")
     image = models.ImageField()
+    # home location or garbage location
+    type = models.CharField()
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=60)
 
-    # time this target started
-    startTime = models.DateTimeField()
     # a default ammount currently
     points = models.IntegerField(default=20)
 
     def create_target(self, loc, info):
         Target.objects.create(self, Location=loc, image=info, points=info,)
         pass
+
+    # returns a list of Targets in range of the current target. maybe.
+    def get_in_range(self):
+
+        # returns the other targets in range of this one TODO currently just does it with all home roles
+        for e in Target.objects.all():
+            if e.type == "Home" and in_range(e, self):
+                targList = targList + e
+        return targList
+
 
     # functionality: if same area is marked by different people can add pictures to the original  target,
     # so no dupes.
--- django_app/app/views.py
+++ django_app/app/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
 
+from django.shortcuts import render
+from django.views import View
+from django_app.app import models
+from rest_framework import serializers
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.generics import ListAPIView
+
+
 # Create your views here.
+from django_app.app.models import Location
+
+
+class LocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Location
+        fields = ["Latitude", "Longitude"]
+
+
+class LocationView(APIView):
+
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
+
+    def post(self, request, format=None):
+        serializer = LocationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LocationsView(ListAPIView):
+
+    def get(self, request, format=None):
+        serializer = LocationSerializer()
